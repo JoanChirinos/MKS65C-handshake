355822f06e01534574c0a1a95e15747f517ac71a
JoanChirinos <jchirinos@stuy.edu>
2018-12-20 21:27:53 -0500

    done??

--- basic_server.c
+++ basic_server.c
@@ -12,30 +12,32 @@ int main() {
 
   signal(SIGINT, sighandler);
 
-  int to_client;
-  int from_client;
-
-  from_client = server_handshake( &to_client );
-
   while (1) {
-    // get client request
-    char* input = calloc(sizeof(char), 200);
-    int got_response = read(from_client, input, 200);
-    printf("got_response: %d\n", got_response);
-    if (got_response == 0) {
-      from_client = server_handshake( &to_client );
-      continue;
+
+    int to_client;
+    int from_client;
+
+    from_client = server_handshake( &to_client );
+
+    if (!fork()) {
+
+      while (1) {
+        // get client request
+        char* input = calloc(sizeof(char), 200);
+        int got_response = read(from_client, input, 200);
+        printf("got_response: %d\n", got_response);
+        printf("got: %s\n", input);
+
+        // make response: response = input + " beans!"
+        char* response = calloc(sizeof(char), 207);
+        strcpy(response, input);
+        strcat(response, " beans!");
+
+        printf("gonna send back: %s\n", response);
+
+        // send back to client
+        write(to_client, response, 207);
+      }
     }
-    printf("got: %s\n", input);
-
-    // make response: response = input + " beans!"
-    char* response = calloc(sizeof(char), 207);
-    strcpy(response, input);
-    strcat(response, " beans!");
-
-    printf("gonna send back: %s\n", response);
-
-    // send back to client
-    write(to_client, response, 207);
   }
 }
